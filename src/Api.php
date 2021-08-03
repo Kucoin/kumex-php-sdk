0ddfec5f8e66e0056bd5ceaa6cc45cf5e03b3738
hugo <hugo.hu@kucoin.com>
2021-08-03 09:49:39 +0800

    update date 2021.08.02

--- src/Api.php
+++ src/Api.php
@@ -21,7 +21,7 @@ abstract class Api
     /**
      * @var string SDK update date
      */
-    const UPDATE_DATE = '2020.08.02';
+    const UPDATE_DATE = '2021.08.02';
 
     /**
      * @var string
